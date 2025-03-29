9dc045df599936a334824df37405f0cb2517f112
minos0908 <arfathimaminos@gmail.com>
2025-03-29 16:38:33 +0530

    Update D2Ex1.java

--- Day-02/Classwork/ex-1/D2Ex1.java
+++ Day-02/Classwork/ex-1/D2Ex1.java
@@ -2,7 +2,7 @@ class D2Ex1
 {
 	public static void main(String args[])
 	{
-		System.out.println("args[0]:"+"args[0]");
-		System.out.println("args[1]:"+"args[1]");
+		System.out.println("args[0]:"+args[0]);
+		System.out.println("args[1]:"+args[1]);
 	}
 }	
