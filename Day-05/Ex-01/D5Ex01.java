79e8c2737944e7d3c76eef873c991dd6421cd655
minos0908 <arfathimaminos@gmail.com>
2025-04-12 22:25:11 +0530

    Add files via upload

---
+++ Day-05/Ex-01/D5Ex01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+  public class D5Ex01{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int[] Number = new int[5];
+		int sum=0;
+		for(int i=0; i<5; i++)
+		{
+			System.out.println("Enter an integer:");
+			Number[i]=sc.nextInt();
+			sum=sum+Number[i];
+		}
+		System.out.println("sum is :"+sum);
+	}
+  }
